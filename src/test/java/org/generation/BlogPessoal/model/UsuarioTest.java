f1cc3a3726b5c0fc52e8720ed3c677e6bfb78147
Agatha Nara <gthlbrc@outlook.com>
2021-09-12 22:31:17 -0300

    JUnit

--- src/test/java/org/generation/BlogPessoal/model/UsuarioTest.java
+++ src/test/java/org/generation/BlogPessoal/model/UsuarioTest.java
@@ -1,8 +1,17 @@
 package org.generation.BlogPessoal.model;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
+import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -15,4 +24,30 @@ public class UsuarioTest {
 	
 	@Autowired
 	private ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+	
+	Validator validator = factory.getValidator(); //esse objeto só terá algo se alguma das coisas na model usuario não estiver correta
+
+	@BeforeEach
+	public void start() {
+		usuario = new Usuario(0L, "João", "joazin12", "12345678");
+	}
+	
+	@Test
+	void testValidaAtributos() {
+		Set<ConstraintViolation<Usuario>> violacao = validator.validate(usuario);
+		System.out.println(violacao.toString());
+		
+		assertTrue(violacao.isEmpty());
+		
+	}
+	
+	@Test
+	void testNaoValidaAtributos() {
+		Set<ConstraintViolation<Usuario>> violacao = validator.validate(usuarioNulo);
+		System.out.println(violacao.toString());
+		
+		assertFalse(violacao.isEmpty());
+		
+	}
+	
 }
